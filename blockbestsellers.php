3010e2bfe8ccb15e1f4aa41cfc8c4fee16f22861
root <selwak@gmail.com>
2015-09-08 15:32:32 +0200

    show bestselers acccrding to language

--- blockbestsellers.php
+++ blockbestsellers.php
@@ -260,7 +260,7 @@ class BlockBestSellers extends Module
 		if (Configuration::get('PS_CATALOG_MODE'))
 			return false;
 
-		if (!($result = ProductSale::getBestSalesLight((int)$params['cookie']->id_lang, 0, (int)Configuration::get('PS_BLOCK_BESTSELLERS_TO_DISPLAY'))))
+		if (!($result = getBestSalesLight((int)$params['cookie']->id_lang, 0, (int)Configuration::get('PS_BLOCK_BESTSELLERS_TO_DISPLAY'))))
 			return (Configuration::get('PS_BLOCK_BESTSELLERS_DISPLAY') ? array() : false);
 
 		$currency = new Currency($params['cookie']->id_currency);
@@ -270,4 +270,81 @@ class BlockBestSellers extends Module
 
 		return $result;
 	}
+}
+
+
+/*
+** Get required informations on best sales products
+**
+** @param integer $id_lang Language id
+** @param integer $page_number Start from (optional)
+** @param integer $nb_products Number of products to return (optional)
+** @return array keys : id_product, link_rewrite, name, id_image, legend, sales, ean13, upc, link
+*/
+
+function getBestSalesLight($id_lang, $page_number = 0, $nb_products = 10, Context $context = null)
+{
+    if (!$context)
+        $context = Context::getContext();
+    if ($page_number < 0) $page_number = 0;
+    if ($nb_products < 1) $nb_products = 10;
+
+    $sql_groups = '';
+    if (Group::isFeatureActive())
+    {
+        $groups = FrontController::getCurrentCustomerGroups();
+        $sql_groups = 'AND cg.`id_group` '.(count($groups) ? 'IN ('.implode(',', $groups).')' : '= 1');
+    }
+
+    //Subquery: get product ids in a separate query to (greatly!) improve performances and RAM usage
+    $products = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
+    SELECT cp.`id_product`
+    FROM `'._DB_PREFIX_.'category_product` cp
+    LEFT JOIN `'._DB_PREFIX_.'category_group` cg ON (cg.`id_category` = cp.`id_category`)
+    WHERE cg.`id_group` '.$sql_groups
+    .' AND cp.`id_category` = 43');
+    
+    $ids = array();
+    foreach ($products as $product)
+        $ids[$product['id_product']] = 1;
+
+    $ids = array_keys($ids);        
+    sort($ids);
+    $ids = count($ids) > 0 ? implode(',', $ids) : 'NULL';
+
+    //Main query
+    $sql = '
+    SELECT
+        p.id_product,  MAX(product_attribute_shop.id_product_attribute) id_product_attribute, pl.`link_rewrite`, pl.`name`, pl.`description_short`, product_shop.`id_category_default`,
+        MAX(image_shop.`id_image`) id_image, il.`legend`,
+        ps.`quantity` AS sales, p.`ean13`, p.`upc`, cl.`link_rewrite` AS category, p.show_price, p.available_for_order, IFNULL(stock.quantity, 0) as quantity, p.customizable,
+        IFNULL(pa.minimal_quantity, p.minimal_quantity) as minimal_quantity, stock.out_of_stock,
+        product_shop.`date_add` > "'.date('Y-m-d', strtotime('-'.(Configuration::get('PS_NB_DAYS_NEW_PRODUCT') ? (int)Configuration::get('PS_NB_DAYS_NEW_PRODUCT') : 20).' DAY')).'" as new
+    FROM `'._DB_PREFIX_.'product_sale` ps
+    LEFT JOIN `'._DB_PREFIX_.'product` p ON ps.`id_product` = p.`id_product`
+    '.Shop::addSqlAssociation('product', 'p').'
+    LEFT JOIN `'._DB_PREFIX_.'product_attribute` pa
+        ON (p.`id_product` = pa.`id_product`)
+    '.Shop::addSqlAssociation('product_attribute', 'pa', false, 'product_attribute_shop.`default_on` = 1').'
+    '.Product::sqlStock('p', 'product_attribute_shop', false, $context->shop).'
+    LEFT JOIN `'._DB_PREFIX_.'product_lang` pl
+        ON p.`id_product` = pl.`id_product`
+        AND pl.`id_lang` = '.(int)$id_lang.Shop::addSqlRestrictionOnLang('pl').'
+    LEFT JOIN `'._DB_PREFIX_.'image` i ON (i.`id_product` = p.`id_product`)'.
+    Shop::addSqlAssociation('image', 'i', false, 'image_shop.cover=1').'
+    LEFT JOIN `'._DB_PREFIX_.'image_lang` il ON (i.`id_image` = il.`id_image` AND il.`id_lang` = '.(int)$id_lang.')
+    LEFT JOIN `'._DB_PREFIX_.'category_lang` cl
+        ON cl.`id_category` = product_shop.`id_category_default`
+        AND cl.`id_lang` = '.(int)$id_lang.Shop::addSqlRestrictionOnLang('cl').'
+    WHERE product_shop.`active` = 1
+    AND p.`visibility` != \'none\'
+    AND p.`id_product` IN ('.$ids.')
+    GROUP BY product_shop.id_product
+    ORDER BY sales DESC
+    LIMIT '.(int)($page_number * $nb_products).', '.(int)$nb_products;
+
+    if (!$result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql))
+        return false;
+
+    return Product::getProductsProperties($id_lang, $result);
 }
